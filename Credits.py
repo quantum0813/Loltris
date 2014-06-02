f2a8c4fec7a6e7c4b4ae53d0d217c16726b0d912
Jonas Møller <jonasmo441@gmail.com>
2014-06-02 11:09:20 +0200

    Added missing module: Draw.py

--- Credits.py
+++ Credits.py
@@ -3,6 +3,7 @@
 import Core
 import Jobs
 import Load
+import Matrix
 import os.path
 from Globals import *
 from pygame.locals import *
@@ -24,6 +25,26 @@ class Credits(Core.Game):
                         fill=MENU_BACKGROUND,
                         )
                     )
+        ## TODO: For this to be possible, Jobs.ScrollingText needs to stop drawing itself when it reaches a certain point,
+        ##       it needs a y-coordinate where the text starts to disappear.
+        # blockwidth = (self.width) // len(TITLE_BLOCKS[0])
+        # self.addJob("title_board",
+        #             Jobs.Board(
+        #                 self,
+        #                 y=SPACER,
+        #                 height=len(TITLE_BLOCKS),
+        #                 width=len(TITLE_BLOCKS[0]),
+        #                 blockwidth=blockwidth,
+        #                 bgcolor=self.bgcolor,
+        #                 queue=100,
+        #                 draw_grid=False,
+        #                 draw_border=False,
+        #                 )
+        #             )
+        # self.jobs.title_board.x = (self.width // 2) - (self.jobs.title_board.width // 2)
+        # for x, y in Matrix.matrixToSet(TITLE_BLOCKS):
+        #     self.jobs.title_board.blocks[(x, y)] = (0xaa,0xaa,0xaa)
+
         self.addJob("endtimer", Jobs.TimedExecution(self.quitGame, timed=False, anykey=True))
 
     def mainLoop(self):
---
+++ Draw.py
@@ -0,0 +1,51 @@
+
+import pygame as Pygame
+
+def fillJob(screen, color, job):
+    if hasattr(job, "fillArea"):
+        job.fillArea(color)
+    else:
+        Pygame.draw.rect(
+                screen,
+                color,
+                (job.x, job.y, job.width, job.height),
+                0)
+
+def draw3DBorder(screen, colors, rect, deepness, background=None):
+    x, y, width, height = rect
+
+    if background:
+        Pygame.draw.rect(
+                screen,
+                background,
+                (x + deepness + 1, y + deepness + 1, width - deepness - 1, height - deepness - 1)
+                )
+
+    ## Top
+    Pygame.draw.polygon(
+            screen,
+            colors[0],
+            ((x, y), (x + width, y),
+             (x + width - deepness, y + deepness), (x + deepness, y + deepness), ),
+            )
+    ## Bottom
+    Pygame.draw.polygon(
+            screen,
+            colors[2],
+            ((x, y + height), (x + width, y + height),
+             (x + width - deepness, y + height - deepness), (x + deepness, y + height - deepness), ),
+            )
+    ## Left
+    Pygame.draw.polygon(
+            screen,
+            colors[3],
+            ((x, y), (x + deepness, y + deepness),
+             (x + deepness, y + height - deepness), (x, y + height),),
+            )
+    ## Right
+    Pygame.draw.polygon(
+            screen,
+            colors[1],
+            ((x + width, y), (x + width - deepness, y + deepness),
+             (x + width - deepness, y + height - deepness), (x + width, y + height)),
+            )
