f8320860f8da15bb62df4606e1053d54b3c8a563
Jonas Møller <jonasmo441@gmail.com>
2014-06-03 13:42:46 +0200

    Updates to Setup.py

--- Setup.py
+++ Setup.py
@@ -6,32 +6,87 @@ import Load
 import Log
 import os.path as Path
 import os as OS
+import shutil as Shutil
 
 SETUP_FILE = ".set_up"
+SETUP_FILE_CONTENTS = """
+# This file tells Loltris that everything has been set up,
+# do not remove this file unless you want to set up everything
+# again (deletes all user-files)
+"""
 
+## Initial contents of Scores.json, an empty array
 SCORES_CONTENTS = """[]"""
 
+def remove(path, follow_links=False):
+    """
+    Acts appropriately to remove a file; be it a directory, link or plain ol' file.
+    """
+    if not Path.exists(path):
+        return
+    if Path.isfile(path) or (Path.islink(path) and follow_links):
+        OS.remove(path)
+        return True
+    if Path.isdir(path):
+        Shutil.rmtree(path)
+        return True
+    if Path.islink(path) and not follow_links:
+        OS.unlink(path)
+        return True
+    return
+
+def mkdir(path, overwrite=False):
+    """
+    Calls OS.mkdir on path, but calls Log.panic in case of an OSError.
+    """
+    try:
+        if overwrite:
+            if remove(path):
+                Log.debug("Removed directory {}, overwrite".format(repr(path)))
+        elif Path.exists(path):
+            return
+
+        OS.mkdir(path)
+        Log.debug("Created directory {}".format(repr(path)))
+    except OSError:
+        Log.panic("Unable to create directory `{}'".format(path))
+
+    return True
+
+def mkfile(path, initial="", overwrite=False):
+    """
+    Creates a new file, overwriting existing files if overwrite is set to True. Calls
+    Log.panic in case of an OSError.
+    """
+    try:
+        if overwrite:
+            if remove(path):
+                Log.debug("Removed file {}, overwrite".format(repr(path)))
+        elif Path.exists(path):
+            return
+
+        with open(path, "wb") as wf:
+            wf.write(initial)
+        Log.debug("Created file {}".format(repr(path)))
+    except OSError:
+        Log.panic("Unable to create file `{}'".format(path))
+
+    return True
+
 def setupFiles():
+    """
+    Sets up necessary files and directories for Loltris
+    """
     if Path.exists(Path.join(Load.DATADIR, SETUP_FILE)):
         return
 
     Log.log("Setting up files for Loltris")
 
-    try:
-        OS.mkdir(Load.HIGHSCOREDIR)
-    except OSError:
-        Log.panic("Unable to create directory `{}'".format(Path.join(Load.HIGHSCOREDIR, "Snapshots")))
+    mkdir(Load.HIGHSCOREDIR, overwrite=True)
+    mkdir(Path.join(Load.HIGHSCOREDIR, "Snapshots"), overwrite=True)
 
-    with open(Path.join(Load.HIGHSCOREDIR, "Scores.json"), "wb") as wf:
-        wf.write(SCORES_CONTENTS)
+    mkfile(Path.join(Load.HIGHSCOREDIR, "Scores.json"), initial=SCORES_CONTENTS, overwrite=True)
+    ## Finally- if everything was set up successfully- create the SETUP_FILE so Loltris
+    ## know that the necessary files/directories have been set up.
+    mkfile(Path.join(Load.DATADIR, SETUP_FILE), SETUP_FILE_CONTENTS, overwrite=True)
 
-    try:
-        OS.mkdir(Path.join(Load.HIGHSCOREDIR, "Snapshots"))
-    except OSError:
-        Log.panic("Unable to create directory `{}'".format(Path.join(Load.HIGHSCOREDIR, "Snapshots")))
-
-    with open(Path.join(Load.DATADIR, SETUP_FILE), "wb") as wf:
-        wf.write("# This file just tells Loltris that everything has been set up,\n" + \
-                 "# do not remove this file unless you want to set up everything\n" + \
-                 "# again (deletes all user-files like scores)\n"
-                 )
